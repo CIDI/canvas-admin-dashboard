f24d109c28095ad47f1d2785d05d77c627cfb53a
Kenneth <kenneth.larsen@usu.edu>
2014-08-20 10:05:27 -0600

    Department graph
    adding in code to give count by department

--- public/js/syllabus.js
+++ public/js/syllabus.js
@@ -93,94 +93,91 @@ function checkTotals() {
         }]
     });
     // Data specific to "All USU Courses"
-    if ($(".retrieveAllCourses:visible").length > 0) {
-        var collegeList = [],
-            collegeTotalCourses = [],
-            collegePublished = [],
-            collegeUnpublished = [],
-            collegeUsingSyllabus = [],
-            collegeNotUsingSyllabus = [];
-        $('h2').each(function () {
-            collegeList.push($(this).text());
-            collegeTotalCourses.push($(this).parents(".college-list").find('li:visible').length);
-            collegePublished.push($(this).parents(".college-list").find('.usingCanvas:visible').length);
-            collegeUnpublished.push($(this).parents(".college-list").find('.notUsingCanvas:visible').length);
-            collegeUsingSyllabus.push($(this).parents(".college-list").find('.fa-check-circle:visible').length);
-            collegeNotUsingSyllabus.push($(this).parents(".college-list").find('.fa-question-circle:visible').length);
-            var myClass = $(this).index();
-                // noSyllabus = $(this).next('.collegeGroup').find('.fa-question-circle').length,
-                // hasSyllabus = $(this).next('.collegeGroup').find('.fa-check-circle').length;
-                // totalCourses = noSyllabus + hasSyllabus,
-                // name = $(this).html();
-            $(this).before('<a name="' + myClass + '"></a>');
-            $(this).find("a").remove();
-            $(this).append('<a class="topLink" href="#top"><i class="icon-circle-arrow-up"></i> Top</a>');
-        });
-        $('#collegeCount').highcharts({
-            chart: {type: 'bar'},
-            colors: [
-                '#B94A48',                '#468847',
-                '#428bca',
-                '#999999',                '#000000'
-            ],
+    var collegeList = [],
+        collegeTotalCourses = [],
+        collegePublished = [],
+        collegeUnpublished = [],
+        collegeUsingSyllabus = [],
+        collegeNotUsingSyllabus = [];
+    $('h2').each(function () {
+        collegeList.push($(this).text());
+        collegeTotalCourses.push($(this).parents(".college-list").find('li:visible').length);
+        collegePublished.push($(this).parents(".college-list").find('.using-canvas:visible').length);
+        collegeUnpublished.push($(this).parents(".college-list").find('.not-using-canvas:visible').length);
+        collegeUsingSyllabus.push($(this).parents(".college-list").find('.fa-check-circle:visible').length);
+        collegeNotUsingSyllabus.push($(this).parents(".college-list").find('.fa-question-circle:visible').length);
+        var myClass = $(this).index();
+            // noSyllabus = $(this).next('.collegeGroup').find('.fa-question-circle').length,
+            // hasSyllabus = $(this).next('.collegeGroup').find('.fa-check-circle').length;
+            // totalCourses = noSyllabus + hasSyllabus,
+            // name = $(this).html();
+        $(this).before('<a name="' + myClass + '"></a>');
+        $(this).append('<a class="topLink" href="#top"><i class="icon-circle-arrow-up"></i> Top</a>');
+    });
+    $('#collegeCount').highcharts({
+        chart: {type: 'bar'},
+        colors: [
+            '#B94A48',                '#468847',
+            '#428bca',
+            '#999999',                '#000000'
+        ],
+        title: {
+            text: 'Canvas Usage'
+        },
+        subtitle: {
+            text: 'By College (Based on courses with student enrollments)'
+        },
+        xAxis: {
+            categories: collegeList,
+            title: {text: null }
+        },
+        yAxis: {
+            min: 0,
             title: {
-                text: 'USU Canvas Usage'
+                text: 'Number of Courses',
+                align: 'high'
             },
-            subtitle: {
-                text: 'By College (Based on courses with student enrollments)'
-            },
-            xAxis: {
-                categories: collegeList,
-                title: {text: null }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Number of Courses',
-                    align: 'high'
-                },
-                labels: {overflow: 'justify'}
-            },
-            tooltip: {valueSuffix: ' Courses'},
-            plotOptions: {
-                bar: {
-                    dataLabels: {
-                        enabled: true
-                    }
+            labels: {overflow: 'justify'}
+        },
+        tooltip: {valueSuffix: ' Courses'},
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
                 }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'top',
-                x: -40,
-                y: 100,
-                floating: true,
-                borderWidth: 1,
-                backgroundColor: '#FFFFFF',
-                shadow: true
-            },
-            credits: {
-                enabled: false
-            },
-            series: [{
-                name: 'No Content in Syllabus Page',
-                data: collegeNotUsingSyllabus
-            }, {
-                name: 'Content in Syllabus Page',
-                data: collegeUsingSyllabus
-            }, {
-                name: 'Courses Published',
-                data: collegePublished
-            }, {
-                name: 'Courses UnPublished',
-                data: collegeUnpublished
-            }, {
-                name: 'Total Courses',
-                data: collegeTotalCourses
-            }]
-        });
-    }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: '#FFFFFF',
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'No Content in Syllabus Page',
+            data: collegeNotUsingSyllabus
+        }, {
+            name: 'Content in Syllabus Page',
+            data: collegeUsingSyllabus
+        }, {
+            name: 'Courses Published',
+            data: collegePublished
+        }, {
+            name: 'Courses UnPublished',
+            data: collegeUnpublished
+        }, {
+            name: 'Total Courses',
+            data: collegeTotalCourses
+        }]
+    });
 }
 $(function () {
     'use strict';
